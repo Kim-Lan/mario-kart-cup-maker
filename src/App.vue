1b9a132b71df39c2e60646ec58f57afd22ef2623
Kim-Lan <me@kim-lan.com>
2023-03-22 10:34:14 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-text-field label="My Custom Cup"></v-text-field>
+
+      <v-expansion-panels>
+        <v-expansion-panel title="Super Mario Kart">
+          <v-expansion-panel-text></v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-main>
+  </v-app>
+</template>
+
+
