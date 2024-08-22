4145251e5047759419b6ccee0aba5913157254ad
Kim-Lan <me@kim-lan.com>
2024-08-22 15:08:38 -0700

    Add dist

--- README.md
+++
@@ -1,5 +0,0 @@
-# Mario Kart 8 DX Cup Maker
-
-A Vue.js web application to create custom Mario Kart 8 DX cups.
-
-Deployed to [GitHub Pages](kim-lan.github.io/mario-kart-cup-maker/)
--- src/stores.js
+++
@@ -1,7 +0,0 @@
-import { ref, reactive } from 'vue';
-
-export const selectedTracks = reactive([{}, {}, {}, {}]);
-
-export const selectedIcon = reactive({});
-
-export const trackCount = ref(0);
